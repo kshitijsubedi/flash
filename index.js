c379a7864ae3d7a278e406b88f5599c7f31cd294
Kshitij Subedi <2subedikshitij@gmail.com>
2024-01-22 15:32:52 +0545

    Refactor email endpoint to handle errors

--- index.js
+++ index.js
@@ -120,21 +120,27 @@ app.use(bodyParser.json());
   });
 
   app.post("/email", async (req, res) => {
-    let page;
-    page = await browser.newPage();
-    await page.setJavaScriptEnabled(req.body.enableJavaScript ?? true);
-    await page.setViewport({
-      width: req.body.screenWidth,
-      height: req.body.screenHeight,
-      deviceScaleFactor: req.body.scale || 1,
-    });
-    await page.waitForTimeout(5000);
-    await page.goto(req.body.url);
+    try {
+      let page;
+      page = await browser.newPage();
+      await page.setJavaScriptEnabled(req.body.enableJavaScript ?? true);
+      await page.setViewport({
+        width: req.body.screenWidth,
+        height: req.body.screenHeight,
+        deviceScaleFactor: req.body.scale || 1,
+      });
+      await page.waitForTimeout(5000);
+      await page.goto(req.body.url);
 
-    const html = await page.content();
-    const regex = /[a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.[a-z]+/g;
-    const emails = html.match(regex);
-    res.json({ emails: emails });
+      const html = await page.content();
+      const regex = /[a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.[a-z]+/g;
+      const emails = html.match(regex);
+      res.json({ emails: emails });
+    } catch (err) {
+      console.error(err);
+      res.writeHead(500);
+      res.end();
+    }
   });
 })();
 
