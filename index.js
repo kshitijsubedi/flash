0562647bacdac95dac1cf483433e1e6140abd676
Kshitij Subedi <2subedikshitij@gmail.com>
2024-02-25 23:42:00 +0545

    Update default screen height to 1920

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.use(bodyParser.json());
       await page.setJavaScriptEnabled(req.body.enableJavaScript ?? true);
       await page.setViewport({
         width: req.body.screenWidth || 1920,
-        height: req.body.screenHeight || 1080,
+        height: req.body.screenHeight || 1920,
         deviceScaleFactor: req.body.scale || 1,
       });
       await page.goto(req.body.url);
